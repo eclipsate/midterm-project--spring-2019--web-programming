62489c7297ecfec823f65a86f5380815826c5e9c
eclipsate <paigemcallister@aol.com>
2019-04-08 22:17:16 -0400

    finished mirror page

--- js/sketch2.js
+++ js/sketch2.js
@@ -1,21 +1,25 @@
 const BACKGROUND_COLOR = `#000`;
-const WWIDTH = 600;
-const HHEIGHT = 300;
+const WWIDTH = 750;
+const HHEIGHT = 475;
 let capture;
 
 function setup() {
     var cnv = createCanvas(WWIDTH, HHEIGHT);
-    cnv.parent('p5-sketch');
-    background(BACKGROUND_COLOR);
+    cnv.parent('p5-sketch2');
+    fill(BACKGROUND_COLOR);
     capture = createCapture(VIDEO)
-      capture.size(600, 500)
+    capture.size(375, 237)
 }
 
 function draw() {
-    //Original video slides horizontally
-      capture.position(mouseX, 0);
-
-      //pixels drawn on canvas using image function remains static and inverted(filter);
-      image(capture, 0, 0, 600, 500);
-      filter(BLUR, 2);
+      capture.position(mouseX, mouseY);
+      image(capture, 0, 0, 750, 475);
+      filter(INVERT);
+      image(capture, 0, 0, 250, 250);
+      filter('GRAY');
+      //end the sketch
+      if (keyIsPressed === true) {
+          noCanvas();
+          capture.position(0, 0);
+      }
 }
